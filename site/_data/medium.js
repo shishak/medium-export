703dda12530ca06a44623e000123577341fc0c0a
Shishak <semenshishak@gmail.com>
2019-01-01 14:27:44 +0200

    Update medium.js

--- site/_data/medium.js
+++ site/_data/medium.js
@@ -10,7 +10,7 @@ module.exports = () => {
         // turn the feed XML into JSON
         toJSON(response.data, function (err, result) {
           // create a path for each item based on Medium's guid URL
-          result.rss.channel[0].item.forEach(element => {
+          result.rss.entry[0].title.forEach(element => {
             var url = element.link[0].split('/');
             element.path = url[url.length-1].split('?')[0];
           });
