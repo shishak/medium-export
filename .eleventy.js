22013516a3fbafee7bd43f9f260ee871ccaab18e
Shishak <semenshishak@gmail.com>
2019-03-06 11:20:19 +0200

    Update .eleventy.js

--- .eleventy.js
+++ .eleventy.js
@@ -1,20 +1,20 @@
 const CleanCSS = require("clean-css");
-const postcss = require('postcss')
-const precss = require('precss')
+
+module.exports = function(eleventyConfig) {
+
+  eleventyConfig.addFilter("date", require("./filters/dates.js") );
+  eleventyConfig.addFilter("cssmin", function(code) {
+    return new CleanCSS({}).minify(code).styles;
+  });
 
 
+  return {
+    dir: {
+      input: "site",
+      output: "dist",
+      data: "_data"
+    },
+    feed: process.env.MEDIUM_FEED ||'https://medium.com/feed/netlify'
+  };
 
-module.exports = function(eleventyConfig) {	
-
-
-  eleventyConfig.addFilter("date", require("./filters/dates.js") );	
-
-
-   return {	  return {
-    dir: {	    dir: {
-      input: "site",	      input: "site",
-      output: "dist",	      output: "dist"
-      data: "_data"	
-    },	    },
-    feed: process.env.MEDIUM_FEED ||'https://medium.com/feed/multiverse-icon-set'	    feed: process.env.MEDIUM_FEED ||'https://medium.com/feed/multiverse-icon-set'
-  };	  };
+};
