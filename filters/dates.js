537035616dc6dc7faa213eb68dd69573a309b78c
Shishak <semenshishak@gmail.com>
2019-03-06 13:29:20 +0200

    Update dates.js

--- filters/dates.js
+++ filters/dates.js
@@ -26,5 +26,5 @@ module.exports = function(date) {
     31 : "st"
   };
   var d = new Date(date);
-  return month[d.getDate()] + " " + d.getMonth() + (ordinal[d.getDate()] || "е");
+  return month[d.getDate()] + " " + d.getDate() + (ordinal[d.getMonth()] || "е");
 }
