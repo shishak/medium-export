97a2153e7e7a43cff4a3d29b407bc53877ee173f
Shishak <semenshishak@gmail.com>
2019-03-06 10:57:28 +0200

    Update dates.js

--- filters/dates.js
+++ filters/dates.js
@@ -1,8 +1,1 @@
-/*
-  A date formatter filter for Nunjucks
-*/
-module.exports = function(date) {
 
-  var d = new Date(date);
-  return month[d.getMonth()] + " " + d.getDate() + (ordinal[d.getDate()] || "th");
-}
