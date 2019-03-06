2950954d595f51dde94ee66bd700a5439eb9db70
Shishak <semenshishak@gmail.com>
2019-03-06 11:02:25 +0200

    Update .eleventy.js

--- .eleventy.js
+++ .eleventy.js
@@ -3,13 +3,3 @@ const postcss = require('postcss')
 const precss = require('precss')
 
 
-
-  return {
-    dir: {
-      input: "site",
-      output: "dist"
-    },
-    feed: process.env.MEDIUM_FEED ||'https://medium.com/feed/multiverse-icon-set'
-  };
-
-};
