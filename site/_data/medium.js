c777ed5c8ae80b939cfd9dd331a1169778f19acd
Shishak <semenshishak@gmail.com>
2019-03-06 11:48:15 +0200

    Update medium.js

--- site/_data/medium.js
+++ site/_data/medium.js
@@ -14,7 +14,7 @@ module.exports = () => {
             var url = element.link[0].split('/');
             element.path = url[url.length-1].split('?')[0];
           });
-          resolve({'url': url, 'post': result.rss.channel[0].item});
+          resolve({'url': url, 'posts': result.rss.channel[0].item});
         });
       })
       .catch((error) => {
