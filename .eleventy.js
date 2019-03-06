bd65719d77a1e4da32cec121b58aadd96d6ecf64
Shishak <semenshishak@gmail.com>
2019-03-06 11:18:04 +0200

    Update .eleventy.js

--- .eleventy.js
+++ .eleventy.js
@@ -3,12 +3,18 @@ const postcss = require('postcss')
 const precss = require('precss')
 
 
- return {	
-    dir: {	
-      input: "site",	
-      output: "dist"	
-    },	
-    feed: process.env.MEDIUM_FEED ||'https://medium.com/feed/multiverse-icon-set'	
-  };	
 
- };
+module.exports = function(eleventyConfig) {	
+
+
+  eleventyConfig.addFilter("date", require("./filters/dates.js") );	
+
+
+   return {	  return {
+    dir: {	    dir: {
+      input: "site",	      input: "site",
+      output: "dist",	      output: "dist"
+      data: "_data"	
+    },	    },
+    feed: process.env.MEDIUM_FEED ||'https://medium.com/feed/multiverse-icon-set'	    feed: process.env.MEDIUM_FEED ||'https://medium.com/feed/multiverse-icon-set'
+  };	  };
